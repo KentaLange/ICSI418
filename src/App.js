72048586f29f74b3f37ef3e1b5b73fe30185ad91
Kenta Lange <59261619+KentaLange@users.noreply.github.com>
2025-02-24 01:13:36 -0500

    Update 2/24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Link to="/Sign_in_HW1">
+      <button>Submit</button>
+    </Link>
   );
 }
 
-export default App;
+function MyButton2() {
+  return (
+    <Link to="/">
+      <button>Submit</button>
+    </Link>
+  );
+}
+
+
+function SignUp() {
+  return (
+    
+    <div>
+      Sign Up
+      
+      <body>
+        <h1>First Name</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>Last Name</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>User ID</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>Password</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <MyButton2 />
+       
+    </body>
+    </div>
+    
+  );
+}
+
+function MainApp() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/Sign_in_HW1" element={<SignUp />} />
+        </Routes>
+      </div>
+
+      <div className="App">
+      <header className="App-header">
+        <h1>User ID</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>Password</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <MyButton />
+      </header>
+    </div>
+    </Router>
+
+  );
+}
+
+export default MainApp;
+
