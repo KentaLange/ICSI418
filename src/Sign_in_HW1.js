450a99eebb73367e87334b28e3e61e2d942315ff
Kenta Lange <59261619+KentaLange@users.noreply.github.com>
2025-02-24 12:42:46 -0500

    Repushing

---
+++ src/Sign_in_HW1.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Sign Up
+      <body>
+        <h1>First Name</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>Last Name</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>User ID</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <h1>Password</h1>
+        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+        <button onclick="location.href='HW1.html'">
+            Submit
+        </button>
+       
+    </body>
+    </div>
+  );
+}
