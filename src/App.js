41f679a8ac50c8b711322b6794d0c0715aefb2b0
Kenta Lange <59261619+KentaLange@users.noreply.github.com>
2025-02-26 23:11:40 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
+//Function that generates a button that links to the Sign_in_HW1 page
 function MyButton() {
   return (
     <Link to="/Sign_in_HW1">
-      <button>Submit</button>
+      <button class="buttonStyle">Submit</button>
     </Link>
   );
 }
-
+//Function that generates a button that links to the Log in page
 function MyButton2() {
   return (
     <Link to="/">
-      <button>Submit</button>
+      <button class="buttonStyle">Submit</button>
     </Link>
   );
 }
 
-
+//Function that generates the Sign Up page
 function SignUp() {
   return (
     
+    //User ID, Last Name, First Name, and Password fields
+    //Submit button that links to the Log in page
     <div>
-      Sign Up
-      
-      <body>
-        <h1>First Name</h1>
-        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
-        <h1>Last Name</h1>
-        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
-        <h1>User ID</h1>
-        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
-        <h1>Password</h1>
-        <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
-        <MyButton2 />
-       
-    </body>
+      <div class="p-3 mb-2 bg-primary text-white">This is our Sign up page</div>
+
+      <div className="App">
+        <header className="App-header">
+          <h1>First Name</h1>
+          <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+          <h1>Last Name</h1>
+          <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+          <h1>User ID</h1>
+          <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+          <h1>Password</h1>
+          <textarea id="w3review" name="w3review" rows="2" cols="50"></textarea>
+          <MyButton2 />
+        </header>
+      </div>
     </div>
     
   );
 }
 
+//Function that generates the Log in page
 function MainApp() {
+  //User ID and Password fields
+  //Submit button that links to the Sign Up page
   return (
     <Router>
       <div>
@@ -49,7 +55,7 @@ function MainApp() {
           <Route path="/Sign_in_HW1" element={<SignUp />} />
         </Routes>
       </div>
-
+      <div class="p-3 mb-2 bg-primary text-white">This is our Log in page</div>
       <div className="App">
       <header className="App-header">
         <h1>User ID</h1>
